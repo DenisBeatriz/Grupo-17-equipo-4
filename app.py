b4c7f8c56799a9881f60ce5848b0214540429ada
Jose Fernando Espinosa <josxfer@gmail.com>
2021-10-27 01:11:31 -0500

    Arreglando las rutas y navegabilidad del usuario

--- app.py
+++ app.py
@@ -1,24 +1,63 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import werkzeug.security as ws, db
 
 app= Flask(__name__)
 app.secret_key = 'mi_llave_secreta'
 
+@app.before_request
+def antes_peticion():
+    if 'email' not in session and request.endpoint in ['home2']:
+        return redirect('/login')
+
 @app.route("/")
-def funcion_home():
+def home():
     return render_template('home1.html')
 
 @app.route("/home2")
-def funcion_home2():
+def home2():
     return render_template('home2.html')
 
 @app.route("/login", methods=['GET', 'POST'])
-def funcion_login():
+def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        print(request.form)
+        email = request.form['email']
+        contrasena = request.form['contrasena']
+
+        registro_usuario = db.obtener_registro('usuario', "email = '{}'".format(email))
+
+        if registro_usuario:
+            contrasena_db = registro_usuario[0][8]
+            inicio_exitoso = ws.check_password_hash(contrasena_db, contrasena)
+
+            if inicio_exitoso:
+                session['email'] = email
+                return redirect('/home2')
+            else:
+                return 'Usuario o contraseña incorrectos' 
+
+        else:
+            return 'El usuario no existe'
+
+@app.route("/registroUsuario", methods=['GET', 'POST'])
+def registroUsuario():
+    if request.method == 'GET':
+        return render_template('registro_usuario.html')
+    else:
+        nombres = request.form['nombres']
+        apellidos = request.form['apellidos']
+        cedula = request.form['cedula']
+        email = request.form['email']
+        celular = request.form['celular']
+        direccion = request.form['direccion']
+        complemento = request.form['complemento']
+        contrasena = request.form['contrasena']
+        conf_contrasena = request.form['conf_contrasena']
+        db.insertar_usuario(nombres, apellidos, cedula, email, celular, direccion, complemento, ws.generate_password_hash(contrasena))
+
         return render_template('home2.html')
+
 
 @app.route("/menu")
 def funcion_menu():
@@ -64,24 +103,12 @@ def funcion_admin():
 def funcion_listaDeseos():
     return render_template('listaDeseos.html')
 
-@app.route("/registroUsuario", methods=['GET', 'POST'])
-def funcion_registro_usuario():
-    if request.method == 'GET':
-        return render_template('registro_usuario.html')
-    else:
-        nombres = request.form['nombres']
-        apellidos = request.form['apellidos']
-        cedula = request.form['cedula']
-        email = request.form['email']
-        celular = request.form['celular']
-        direccion = request.form['direccion']
-        complemento = request.form['complemento']
-        contrasena = request.form['contrasena']
-        conf_contrasena = request.form['conf_contrasena']
-        db.insertar_usuario(nombres, apellidos, cedula, email, celular, direccion, complemento, ws.generate_password_hash(contrasena))
+@app.route("/cerrar_sesion")
+def cerrar_sesion():
+    if 'email' in session:
+        session.pop('email')
 
-        return render_template('home2.html')
-
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
